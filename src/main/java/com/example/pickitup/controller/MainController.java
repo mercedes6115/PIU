895eb8958a7ddcba3a361fc9ea30ddec9a187cea
MinByeongHyeon <min_6652@naver.com>
2022-06-17 17:18:45 +0900

    민병현 변경

--- src/main/java/com/example/pickitup/controller/MainController.java
+++ src/main/java/com/example/pickitup/controller/MainController.java
@@ -20,14 +20,9 @@ public class MainController {
     // 메인페이지
     @GetMapping("/main")
     public String main(Model model){
-
-//        model.addAttribute("projectList", projectFileService.getList());   // 사진 가져오기
-//        model.addAttribute("projectList", projectService.getList());  // 내용가져오기
-//        model.addAttribute("projectList", projectService.getJjimNum());  // 찜 갯수
-
-//        model.addAttribute("projectList", projectService.getJoinNum());  // 참여인원 수
-
-//        model.addAttribute("projectList", projectService.getPointNum());  // 포인트 양
+        log.info(projectService.getListJJim().toString());
+//        model.addAttribute("projectListThumb", projectFileService.getList());   // 사진 가져오기
+        model.addAttribute("projectListJJim", projectService.getListJJim());  // 내용가져오기
 
         return "/main/main";
     }
