79a964f183a91ddfb341a7d3287e3216ac1779b3
koreaithw <aangie4@naver.com>
2022-06-22 15:58:29 +0900

    파일 업로드 완료! 김현엽 By 서민성

--- src/main/java/com/example/pickitup/service/product/productReview/ProductReviewFileService.java
+++ src/main/java/com/example/pickitup/service/product/productReview/ProductReviewFileService.java
@@ -14,8 +14,8 @@ public class ProductReviewFileService {
     private final ProductReviewFileDAO productReviewFileDAO;
 
     // 파일 경로 가져오기(mapper 매서드명 수정)
-    public List<ProductReviewFileVO> findByProductReviewNum(Long productReviewNum) {
-        return productReviewFileDAO.findByProductReviewNum(productReviewNum);
+    public List<ProductReviewFileVO> findByProductReviewNum(Long reviewNum) {
+        return productReviewFileDAO.findByProductReviewNum(reviewNum);
     }
 
     // 해당 후기글 작성 도중 창을 이동할 경우 저장 경로에서 사진 지우기
@@ -29,8 +29,8 @@ public class ProductReviewFileService {
     };
 
     // 사진 삭제
-    public void remove(Long productReviewNum){
-        productReviewFileDAO.remove(productReviewNum);
+    public void remove(Long reviewNum){
+        productReviewFileDAO.remove(reviewNum);
     }
 
 }
