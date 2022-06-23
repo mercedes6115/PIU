cb0146e1791b004b979095f7ad9534410b5ac4a3
KimYunGyu2 <kyg839912@naver.com>
2022-06-23 15:44:23 +0900

    수정

--- src/main/java/com/example/pickitup/controller/AdminController.java
+++ src/main/java/com/example/pickitup/controller/AdminController.java
@@ -98,7 +98,7 @@ public class AdminController {
     // 관리자 adminboard 체크 삭제
     @ResponseBody
     @PostMapping("/deleteById")
-    public String deleteById(Long num, Model model, AdminCriteria adminCriteria, HttpServletRequest request){
+    public String deleteById(Long num, HttpServletRequest request){
         String[] ajaxMsg = request.getParameterValues("valueArr");
         int size = ajaxMsg.length;
         for(int i = 0; i<size; i++){
