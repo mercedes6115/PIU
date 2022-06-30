4fa5326440f0b3abc29e6003784b1d24cbcb7ce1
garlic3 <yup_yup@naver.com>
2022-06-30 09:56:35 +0900

    프로젝트 2차 병합

--- src/main/java/com/example/pickitup/service/user/ApplyService.java
+++ src/main/java/com/example/pickitup/service/user/ApplyService.java
@@ -1,6 +1,7 @@
 package com.example.pickitup.service.user;
 
 import com.example.pickitup.domain.dao.user.ApplyDAO;
+import com.example.pickitup.domain.vo.dto.ApplyDTO;
 import com.example.pickitup.domain.vo.project.projectFile.ProjectVO;
 import com.example.pickitup.domain.vo.user.ApplyVO;
 import lombok.RequiredArgsConstructor;
@@ -13,14 +14,13 @@ import java.util.List;
 public class ApplyService {
 
     private final ApplyDAO applyDAO;
-
     // 내가 신청한 프로젝트 목록
     public List<ProjectVO> getList(Long userNum){
         return applyDAO.getList(userNum);
     }
 
     // 프로젝트 신청
-    public void register(ApplyVO applyVO){
+    public void apply(ApplyVO applyVO){
         applyDAO.register(applyVO);
     }
 
@@ -29,8 +29,23 @@ public class ApplyService {
         return applyDAO.read(num);
     }
 
-    // 프로젝트 참가자 정보 수정(완수여부)
-    public boolean update(Long approach, Long userNum){
-        return applyDAO.update(approach, userNum);
+    // 유저 프로젝트 완료
+    public void userFinish(Long num){
+        applyDAO.userFinish(num);
+    }
+
+    // 유저 프로젝트 시작
+    public void userStart(ApplyDTO applyDTO){
+        applyDAO.userStart(applyDTO);
+    }
+
+    // 유저 프로젝트 종료
+    public void userEnd(ApplyDTO applyDTO){
+        applyDAO.userEnd(applyDTO);
+    }
+
+    // 신청한 유저 목록
+    public List<ApplyDTO> getApplyUser(Long projectNum){
+        return applyDAO.getApplyUser(projectNum);
     }
 }
